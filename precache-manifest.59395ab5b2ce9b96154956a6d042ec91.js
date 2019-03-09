2cda423d440b458563e34291cb93f8ed917cecf1
Aakarsh <aakarshs@outlook.com>
2019-03-08 21:56:32 -0500

    Updates

---
+++ precache-manifest.59395ab5b2ce9b96154956a6d042ec91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5aab7eaccc551fe900820e01f85dc173",
+    "url": "/SocialNetwork/static/media/egramLogo.5aab7eac.png"
+  },
+  {
+    "revision": "00258ff07e397cfed993ab84f58abb80",
+    "url": "/SocialNetwork/static/media/cancel.00258ff0.png"
+  },
+  {
+    "revision": "12f5f4c8b872f0821ad7",
+    "url": "/SocialNetwork/static/js/runtime~main.12f5f4c8.js"
+  },
+  {
+    "revision": "da79cfd579e94a7ba956",
+    "url": "/SocialNetwork/static/js/main.da79cfd5.chunk.js"
+  },
+  {
+    "revision": "99950afef7539872c3f5",
+    "url": "/SocialNetwork/static/js/2.99950afe.chunk.js"
+  },
+  {
+    "revision": "da79cfd579e94a7ba956",
+    "url": "/SocialNetwork/static/css/main.3624aeb9.chunk.css"
+  },
+  {
+    "revision": "c48fbf746f51115453ff9b93c0d15881",
+    "url": "/SocialNetwork/index.html"
+  }
+];
